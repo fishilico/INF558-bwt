191a60d5047fb3fc4f49aa19478ff5cb8226c4a7
Guilhem Pujol <guilhem.pujol@polytechnique.edu>
2012-12-17 02:57:13 +0100

    Huffman encoding
    Signed-off-by: Guilhem Pujol <guilhem.pujol@polytechnique.edu>

---
+++ HuffmanTreeComparator.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+
+public class HuffmanTreeComparator implements Comparator<HuffmanTree> {
+
+	@Override
+	public int compare(HuffmanTree arg0, HuffmanTree arg1) {
+		if(arg0.weight < arg1.weight) return -1;
+		else if(arg0.weight > arg1.weight) return 1;
+		return 0;
+	}
+
+}
